0150372633915289f5e0aefc732dc52bc8999e66
Aaditya1103 <149060958+Aaditya1103@users.noreply.github.com>
2025-02-17 17:55:45 -0700

    Add files via upload

---
+++ partials/_footer.php
@@ -0,0 +1,3 @@
+<div class="footer container-fluid bg-dark text-light">
+    <p class="text-center py-2 mb-0">Designed by <a href="https://www.linkedin.com/in/aaditya-sakhardande-980668206/" target="_blank" rel="noopener noreferrer">@aadityasakhardande</a></p>
+</div>
